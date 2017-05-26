3e8535b37b764ed9f15a720c1884cec16d94e910
olga-golentova <golenolg@gmail.com>
2017-05-26 21:03:50 +0300

    Calculator added and first steps of Android projet

---
+++ Lab2/Calculator/RedCell.App.Calculator.Example/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace RedCell.App.Calculator.Example
+{
+    public partial class Form1 : Form
+    {
+        private double accumulator = 0;
+        private char lastOperation;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Operator_Pressed(object sender, EventArgs e)
+        {   // .Нажат оператор - произвести последнюю операциб и сохранить новый оператор
+         
+            char operation = (sender as Button).Text[0];
+            if (operation == 'C')
+            {
+                accumulator = 0;
+            }
+            else
+            {
+                double currentValue = double.Parse(Display.Text);
+                switch (lastOperation)
+                {
+                    case '+': accumulator += currentValue; break;
+                    case '-': accumulator -= currentValue; break;
+                    case '×': accumulator *= currentValue; break;
+                    case '÷': accumulator /= currentValue; break;
+                    default: accumulator = currentValue; break;
+                }
+            }
+
+            lastOperation = operation;
+            Display.Text = operation == '=' ? accumulator.ToString() : "0";
+        }
+
+        private void Number_Pressed(object sender, EventArgs e)
+        {
+            // Вывести это на дисплей
+            string number = (sender as Button).Text;
+            Display.Text = Display.Text == "0" ? number : Display.Text + number;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
